f13c590a7552d8d76fbacdc8506f0d98873d6053
rashbunn <131571752+lasystuff@users.noreply.github.com>
2026-09-23 10:04:11 +0900

    changes

--- core/autoload/global.gd
+++ core/autoload/global.gd
@@ -7,7 +7,7 @@ var root:Window:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if !Engine.is_embedded_in_editor():
-		get_window().size *= DisplayServer.screen_get_scale()
-		get_window().move_to_center()
+	#if !Engine.is_embedded_in_editor():
+	#	get_window().size *= DisplayServer.screen_get_scale()
+	#	get_window().move_to_center()
 	rng = RandomNumberGenerator.new()
